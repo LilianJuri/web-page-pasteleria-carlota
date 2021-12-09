444d090af3844b0a43e2ca2fc1916b40e54af289
Lilian Juri <lilianjuri17@gmail.com>
2021-12-09 15:20:03 -0300

    add changes background image of the page inicio and css text

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,14 +1,23 @@
+import { Link } from "react-router-dom";
 import './Inicio.css';
+import BImage from "../img/background-inicio.jpg";
 
 
 const Inicio = () => {
     return(
-        <div className='background-image-inicio'>
-            <div className="text-inicio">
-                <h1 className >Pasteleria Carlota</h1>
-                <p class>La pasteleria mas dulce y copada de todas</p>
+            <div className='inicio'>
+                <div className="header-container" style={{ backgroundImage: `url(${BImage})`}}>
+                    <div className="text-conteiner" >
+                        <h1>Pasteleria Carlota</h1>
+                        <p>La pasteleria mas dulce y copada de todas </p>
+                        <p>e insoportablemente densa</p>
+                    <Link to="/menu">
+                        <button> Productos </button>
+                    </Link>
+                    </div>
+                </div>
             </div>
-        </div>
+    
     )
 }
 
