458a81251cb883a0e92fa12e9acc906d218ceec7
Lilian Juri <lilianjuri17@gmail.com>
2021-12-11 20:07:32 -0300

    add changes in page sobrenosotros

--- src/pages/SobreNosotros.js
+++ src/pages/SobreNosotros.js
@@ -1,10 +1,33 @@
+
+import bsobreNosotros from "../img/background-sobre-nosotros.jpg";
+import './SobreNosotros.css';
+
 const SobreNosotros = () => {
     return(
-        <div>
-            <h1>
-                Sobre nosotros
-            </h1>
+        <div className='sobre-nosotros'>
+        <div className="header-container" style={{ backgroundImage: `url(${bsobreNosotros})`}}>
+            <div className="text-conteiner" >
+                <h1>Sobre Nosotros</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Cras ac magna quis metus molestie luctus. Duis facilisis 
+                    urna nec odio lacinia tempor. Nullam vulputate feugiat diam 
+                    vel venenatis. Fusce consequat commodo ligula, in iaculis dui 
+                    scelerisque in. Curabitur commodo lorem ac elit imperdiet, et 
+                    tristique felis cursus. Phasellus congue aliquet ullamcorper. 
+                    Vivamus id dapibus lorem. Donec molestie, est vel vulputate 
+                    cursus, nunc quam interdum leo, nec congue lorem sem ut leo.
+                    Vestibulum interdum, est vel consectetur malesuada, ipsum nulla
+                    hendrerit mi, eu gravida tellus mauris eu odio. Mauris sagittis
+                    enim dui, sed suscipit enim vestibulum id. Vivamus rhoncus 
+                    imperdiet neque a eleifend. Phasellus luctus, leo mollis dapibus
+                    ornare, magna sapien sollicitudin odio, non ornare tortor dui in lacus.
+                    Proin gravida eros ornare, suscipit magna non, consectetur eros. 
+                    Curabitur rhoncus libero eget odio molestie tempus. Praesent rutrum 
+                    tellus vitae ipsum tincidunt hendrerit.
+                    </p>
+            </div>
         </div>
+    </div>
     )
 }
 
