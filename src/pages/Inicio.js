699dc62cfd2113aa4cb5a51167ca0aee5dcc24b4
Lilian Juri <lilianjuri17@gmail.com>
2021-12-15 15:52:57 -0300

    add responsive font size

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -6,11 +6,10 @@ import './Inicio.css';
 const Inicio = () => {
     return(
             <div className='inicio'>
-                <div className="header-container-i" >
+                <div className="inicio-container-i" >
                     <div className="text-conteiner-i" >
                         <h1>Pasteleria Carlota</h1>
-                        <p>La pasteleria mas dulce y copada de todas </p>
-                        <p>e insoportablemente densa</p>
+                        <p>La pasteleria mas dulce y copada de todas <br/>e insoportablemente densa </p>
                     <Link to="/productos">
                         <button> Productos </button>
                     </Link>
